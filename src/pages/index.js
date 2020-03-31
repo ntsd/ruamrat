09ef7f726d5c28662f661f1657f26b07aec3c043
ntsd <jo06942@gmail.com>
2020-03-31 09:32:47 +0700

    Add hotcode maintenance page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,22 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 import Hero from '../components/hero'
 import Section from '../components/section'
+import { MaintenancePage } from '@hotcode/maintenance-page'
 
 const IndexPage = ({ data }) => (
-  <Layout>
-    <SEO title="รวมรัตน์" keywords={ [`รวมรัตน์`, `กระจกอลูมิเนียม`, `ติดตั้งกระจกอลูมิเนียม`] } />
-    <Hero fluid={ data.hero.childImageSharp.fluid }>
-    </Hero>
-    <Section>test
-    </Section>
-    <Section>test
-    </Section>
-  </Layout>
+  true
+    ?
+      <MaintenancePage color="red"></MaintenancePage>
+    :
+      <Layout>
+        <SEO title="รวมรัตน์" keywords={[`รวมรัตน์`, `กระจกอลูมิเนียม`, `ติดตั้งกระจกอลูมิเนียม`]} />
+        <Hero fluid={data.hero.childImageSharp.fluid}>
+        </Hero>
+        <Section>
+        </Section>
+        <Section>
+        </Section>
+      </Layout>
 )
 
 export default IndexPage
