5c98809b5c49e856e74b5de9d49b683e47d8fead
ntsd <jo06942@gmail.com>
2020-05-16 19:29:13 +0700

    Remove old files

--- src/components/footer/footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Footer: React.FC = () => {
-  return (
-    <footer style={{textAlign: 'center'}}>
-      Copyright © {new Date().getFullYear()} <a href="https://hotcode.dev">Hotcode</a>
-    </footer>
-  )
-}
