7e2f6d9f625506f4b99df0567a2787ebf1b3cd52
ntsd <jo06942@gmail.com>
2020-05-23 06:29:55 +0700

    Add description long

--- src/components/DescriptionCard/DescriptionCard.tsx
+++ src/components/DescriptionCard/DescriptionCard.tsx
@@ -5,13 +5,14 @@ export interface DescriptionCardProps {
   title: string;
   title2?: string;
   description: any;
+  descriptionLong?: boolean;
   noCrown?: boolean;
   titleStyle?: object;
   footer?: any;
   style?: React.CSSProperties;
 }
 
-export const DescriptionCard: React.FC<DescriptionCardProps> = ({ title, title2, description, noCrown, footer, titleStyle, style }) => {
+export const DescriptionCard: React.FC<DescriptionCardProps> = ({ title, title2, description, noCrown, footer, titleStyle, style, descriptionLong = false }) => {
   return (
     <div className="card-container" style={style}>
       {
@@ -23,7 +24,7 @@ export const DescriptionCard: React.FC<DescriptionCardProps> = ({ title, title2,
         title2 &&
         <h1 className="title" style={titleStyle}> {title2} </h1>
       }
-      <p className='description'>{description}</p>
+      <p className={descriptionLong ? 'description-long' : 'description'}>{description}</p>
       <div className="footer">{ footer }</div>
     </div>
   )
