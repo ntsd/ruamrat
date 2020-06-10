49ce8619c9be97499ce6fcc7e8d29388d1a7d2fb
ntsd <jo06942@gmail.com>
2020-06-10 15:26:18 +0700

    Fix seo

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -18,6 +18,7 @@ export const Image: React.FC<ImageProps> = ({
       className={`${withFrame && 'img-frame'}`}
       fluid={fluid}
       style={style}
+      alt="รวงมรัตน์ กระจก อลูมิเนียม"
     />
   );
 };
