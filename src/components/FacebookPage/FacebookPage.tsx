c6f6dce3b5641a8417107e7b49eac20a2e1820eb
ntsd <jo06942@gmail.com>
2020-06-10 15:04:03 +0700

    Update facebook

--- src/components/FacebookPage/FacebookPage.tsx
+++ src/components/FacebookPage/FacebookPage.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
 
 export const FacebookPage: React.FC = () => {
+  const width = '332';
+  const height = '400';
+  const pageUrl = 'https%3A%2F%2Fwww.facebook.com%2Fruamrat%2F';
+  const appId = '606471186918943';
+  const showFacepile = false;
+  const hideCover = true;
+  const smallHeader = false;
+  const adaptContainerWidth = true;
   const src =
-    'https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fruamrat%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=1261066970649914';
+    'https://www.facebook.com/plugins/page.php?tabs=timeline' +
+    `&href=${pageUrl}` +
+    `&width=${width}` +
+    `&height=${height}` +
+    `&small_header=${smallHeader}` +
+    `&adapt_container_width=${adaptContainerWidth}` +
+    `&hide_cover=${hideCover}` +
+    `&show_facepile=${showFacepile}` +
+    `&appId=${appId}`;
   return (
     <iframe
       src={src}
-      width="340"
-      height="500"
+      width={width}
+      height={height}
       style={{border: 'none', overflow: 'hidden'}}
       scrolling="no"
       allowTransparency={true}
