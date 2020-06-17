33db0744e673f712a23b15925ccb1e5b4bcfb6ce
ntsd <jo06942@gmail.com>
2020-06-18 03:44:35 +0700

    Update iframe load delay 5 sec

--- src/components/GoogleMap/GoogleMap.tsx
+++ src/components/GoogleMap/GoogleMap.tsx
@@ -4,7 +4,9 @@ export const GoogleMap: React.FC = () => {
   const [showIframe, setShowIframe] = useState(false);
 
   useEffect(() => {
-    setShowIframe(true);
+    setTimeout(() => {
+      setShowIframe(true);
+    }, 5000);
   });
 
   return (
